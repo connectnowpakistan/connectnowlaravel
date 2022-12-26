5f5b6dd7138f028a6e40135e9df352d4bb9793db
Usama Arslan <usama.arslan@softpyramid.com>
2022-12-26 16:57:35 +0500

    initial commit

---
+++ src/Actions/SendMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Softpyramid\ItelSms\Actions;
+
+use Illuminate\Support\Facades\Http;
+
+
+class SendMessage
+{
+    public function execute($params)
+    {
+        $baseUrl = isset($params['local']) && $params['local'] ? config('sms.localUrl') : config('sms.baseUrl');
+        $res = Http::post($baseUrl . 'sendSms', [
+            'api_key' => config('sms.apiKey'),
+            'api_secret' => config('sms.apiSecret'),
+            'receiver_phone' => $params['phone'],
+            'message' => $params['message']
+        ]);
+        return $res->body();
+    }
+}
---
+++ src/config/sms.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'apiKey' => env('API_KEY', null),
+    'apiSecret' => env('API_SECRET', null),
+    'baseUrl' => "https://messaging.projects.softpyramid.com/api/v1/",
+    'localUrl' => "http://messagingsystem.test/api/v1/"
+];
